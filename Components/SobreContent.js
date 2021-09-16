1f4053535335cdd846be2f2707f603600f3c7f15
lv1500 <65210283+lv1500@users.noreply.github.com>
2021-09-16 16:09:30 -0300

    Sobre Começo
    Começando o desenvolvimento do sobre

--- Components/SobreContent.js
+++ Components/SobreContent.js
@@ -4,12 +4,32 @@ const SobreContent = () => {
 
     return (
         <section className={styles.container}>
+            <div className={styles.aboutUs}>
+                <img src='https://th.bing.com/th/id/OIP.Hf0ZaY_J7Vlklow0ECb0-gHaD4?w=304&h=180&c=7&r=0&o=5&pid=1.7'/>
+                <h2>Quem Somos</h2>
+                <p>Subtitulo Subtitulo Subtitulo Subtitulo Subtitulo </p>
+                <div>
+                    <p className={styles.paboutUs}>Descrição da Esquerda Descrição da Esquerda Descrição da Esquerda Descrição da Esquerda Descrição da Esquerda Descrição da Esquerda Descrição da Esquerda</p>
+                    <p className={styles.paboutUs}>Descrição da Direita v Descrição da Direita Descrição da Direita Descrição da Direita Descrição da Direita Descrição da Direita</p>                
+                </div>
+            </div>
 
-            <span>EM DESENVOLVIMENTO</span>
-            <span>EM DESENVOLVIMENTO</span>
-
-
-
+            <div className={styles.quantidade}>
+                <h2>20</h2>
+                <p>Artigos</p>
+            </div>
+            <div className={styles.quantidade}>
+                <h2>01</h2>
+                <p>E-book</p>
+            </div>
+            <div className={styles.quantidade}>
+                <h2>20</h2>
+                <p>Artigos</p>
+            </div>
+            <div className={styles.quantidade}>
+                <h2>1</h2>
+                <p>E-book</p>
+            </div>
         </section>
     );
 }
